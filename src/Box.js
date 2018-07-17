2a31d3f8fd4ff530fe01daa01c9d5df19e0590c7
Robin Malgoire <robin.malgoire@team.belong.com.au>
2018-07-17 11:29:13 +1000

    master | set prop-types for Box component

--- src/Box.js
+++ src/Box.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import * as boxProps from './Box.props';
 import { isObject, intersectBoxKeys } from './Box.helpers';
 import { media } from './Box.mixins';
+import PropTypes from 'prop-types';
 
 const Box = styled.div`
   ${props => {
@@ -22,5 +23,41 @@ const Box = styled.div`
     })
   }}
 `;
+
+const objectOr = (other) => {
+  return PropTypes.oneOfType([].concat(
+    [PropTypes.object],
+    other
+  ));
+}
+
+Box.propTypes = {
+  as: PropTypes.string,
+  padding: objectOr([PropTypes.array]),
+  paddingTop: objectOr([PropTypes.number, PropTypes.string]),
+  paddingRight: objectOr([PropTypes.number, PropTypes.string]),
+  paddingBottom: objectOr([PropTypes.number, PropTypes.string]),
+  paddingLeft: objectOr([PropTypes.number, PropTypes.string]),
+  margin: objectOr([PropTypes.array]),
+  marginTop: objectOr([PropTypes.number, PropTypes.string]),
+  marginRight: objectOr([PropTypes.number, PropTypes.string]),
+  marginBottom: objectOr([PropTypes.number, PropTypes.string]),
+  marginLeft: objectOr([PropTypes.number, PropTypes.string]),
+  border: objectOr([PropTypes.array]),
+  radius: objectOr([PropTypes.array]),
+  background: objectOr([PropTypes.string]),
+  color: objectOr([PropTypes.string]),
+  textAlign: objectOr([PropTypes.string]),
+  templates: objectOr([PropTypes.string]),
+  columns: objectOr([PropTypes.number]),
+  column: objectOr([PropTypes.number]),
+  row: objectOr([PropTypes.number]),
+  width: objectOr([PropTypes.number, PropTypes.string]),
+  minWidth: objectOr([PropTypes.number, PropTypes.string]),
+  maxWidth: objectOr([PropTypes.number, PropTypes.string]),
+  height: objectOr([PropTypes.number, PropTypes.string]),
+  minHeight: objectOr([PropTypes.number, PropTypes.string]),
+  maxHeight: objectOr([PropTypes.number, PropTypes.string]),
+};
 
 export default Box;
