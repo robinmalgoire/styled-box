0b651175fdbe0b2a7f9c62c9f19b91efc1f7bb72
Robin Malgoire <robin.malgoire@team.belong.com.au>
2018-08-14 13:40:03 +1000

    master | proxy file is not using the correct path

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-export { intersectBoxKeys as intersectBoxKeys } from './src/styled-box/Box.helpers';
-export { intersectBoxProps as intersectBoxProps } from './src/styled-box/Box.helpers';
-export { default as default } from './src/styled-box/Box';
+export { default as default } from './src/Box/Box';
